f1bf337b288afd74e3bcf27c93280b5fa217e4d8
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2023-10-20 21:28:22 -0300

    Update type.js

--- scripts/type.js
+++ scripts/type.js
@@ -1,1 +1,14 @@
+        i = 0
+        cont = 'Projeto Alpha Iniciado...'
+        txt = document.querySelector('.title')
+        vel = 50
+        
+        function Typing() {
+            if (i < cont.length) {
+                txt.innerHTML += cont.charAt(i)
+                i++
+                setTimeout(Typing, vel)
+            }
+        }
 
+        window.addEventListener('load', Typing)
