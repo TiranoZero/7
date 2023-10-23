3c5b7a87cfd264d3a21b0cc81b3ca9472c3aa3a6
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2023-10-23 19:26:43 -0300

    Update type.js

--- scripts/type.js
+++ scripts/type.js
@@ -1,14 +1,22 @@
-        i = 0
-        cont = 'Projeto Alpha Iniciado...'
-        txt = document.querySelector('.title')
-        vel = 50
-        
-        function Typing() {
-            if (i < cont.length) {
-                txt.innerHTML += cont.charAt(i)
-                i++
-                setTimeout(Typing, vel)
-            }
-        }
+typer = document.querySelector('.main')
+cd = document.querySelector('.cdown')
+frase = 'Projeto Alpha Iniciado...'
+inputContainer = document.querySelector('.title')
+vel = 40
+i = 0
 
-        window.addEventListener('load', Typing)
+function Typing() {
+    if (i < frase.length) {
+        inputContainer.innerHTML += frase.charAt(i)
+        i++
+        setTimeout(Typing, vel)
+    }    
+    else {
+        show = ()=>{
+        typer.style.display = "none"
+        cd.style.display = "block"}
+        setTimeout(show, 2000)
+    }
+}
+
+window.addEventListener('load', Typing)
