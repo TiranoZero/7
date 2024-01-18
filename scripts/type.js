434404415757d0d34e2864387476237f926fbf06
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-18 20:27:09 -0300

    ver

--- scripts/type.js
+++ scripts/type.js
@@ -1,23 +1,15 @@
-typer = document.querySelector('.main')
-cd = document.querySelector('.cabec')
 txt = 'Projeto Alpha Iniciado...'
 inputContainer = document.querySelector('.title')
 
 vel = 40
 i = 0
 
-function Typing(onde,frase,loader,content) {
+function Typing(onde,frase) {
     if (i < frase.length) {
         onde.innerHTML += frase.charAt(i)
         i++
-        setTimeout(Typing(onde, frase,loader, content), vel)
+        setTimeout(Typing(onde, frase), vel)
     }    
-    else {         
-        setTimeout(()=>{
-            loader.style.display = "none"
-            content.style.display = "block"},
-            2000)
-    }
 }
 
-window.addEventListener('load', Typing(inputContainer, txt, typer, cd))
+window.addEventListener('load', Typing(inputContainer, txt))
