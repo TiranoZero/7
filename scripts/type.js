25e699ac7197b6ec97a7c2476de7c0c3bf3bde4f
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-18 20:46:29 -0300

    kkkkk

--- scripts/type.js
+++ scripts/type.js
@@ -1,19 +1,20 @@
-txt = 'Bem vindo, seja você viajate ou não, como posso explicar... <br> Talvez você já tenha escutado algo sobre viagem no tempo né? eu suponho que sim, se este tema te interessa faz sentido que tenha vindo parar aqui. Logo sua memória voltará aos poucos, a minha levou cerca de 15 anos para estabilizar. O site tem o intúito de entreter e atualizar os viajantes, para as pessoas desta era, será entretenimento, para nós será nossa missão, até breve.'
+txt = 'Bem vindo, seja você viajate ou não, como posso explicar... Talvez você já tenha escutado algo sobre viagem no tempo né? eu suponho que sim, se este tema te interessa faz sentido que tenha vindo parar aqui. Logo sua memória voltará aos poucos, a minha levou cerca de 15 anos para estabilizar. O site tem o intúito de entreter e atualizar os viajantes, para as pessoas desta era, será entretenimento, para nós será nossa missão, até breve.'
 
 el = document.querySelector('.saud')
 
 vel = 40
 i = 0
 
-function Typing(onde,frase) {
-    if (i < frase.length) {
-        onde.innerHTML += frase.charAt(i)
-        i++
-        setTimeout(Typing(onde, frase), vel)
-    }    
-}
+
 
 function main(){
+    function Typing(onde,frase) {
+        if (i < frase.length) {
+            onde.innerHTML += frase.charAt(i)
+            i++
+            setTimeout(Typing(onde, frase), vel)
+        }    
+    }
     Typing(el, txt)
 }
 
