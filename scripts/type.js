7860bb808dc58ddb78bd2aa2be3ca2c39e115d53
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-18 20:17:46 -0300

    atualizado

--- scripts/type.js
+++ scripts/type.js
@@ -1,22 +1,23 @@
 typer = document.querySelector('.main')
-cd = document.querySelector('.cdown')
-frase = 'Projeto Alpha Iniciado...'
+cd = document.querySelector('.cabec')
+txt = 'Projeto Alpha Iniciado...'
 inputContainer = document.querySelector('.title')
+
 vel = 40
 i = 0
 
-function Typing() {
+function Typing(onde,frase,loader,content) {
     if (i < frase.length) {
-        inputContainer.innerHTML += frase.charAt(i)
+        onde.innerHTML += frase.charAt(i)
         i++
-        setTimeout(Typing, vel)
+        setTimeout(Typing(onde, frase,loader, content), vel)
     }    
-    else {
-        show = ()=>{
-        typer.style.display = "none"
-        cd.style.display = "block"}
-        setTimeout(show, 2000)
+    else {         
+        setTimeout(()=>{
+            loader.style.display = "none"
+            content.style.display = "block"},
+            2000)
     }
 }
 
-window.addEventListener('load', Typing)
+window.addEventListener('load', Typing(inputContainer, txt, typer, cd))
