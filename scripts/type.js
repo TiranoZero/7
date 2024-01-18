828557789ff085f0505ac37b6df3125522f50fd5
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-18 20:40:47 -0300

    gi

--- scripts/type.js
+++ scripts/type.js
@@ -1,4 +1,4 @@
-txt = 'Projeto Alpha Iniciado...'
+txt = 'Bem vindo, seja você viajate ou não, como posso explicar... <br> Talvez você já tenha escutado algo sobre viagem no tempo né? eu suponho que sim, se este tema te interessa faz sentido que tenha vindo parar aqui. Logo sua memória voltará aos poucos, a minha levou cerca de 15 anos para estabilizar. O site tem o intúito de entreter e atualizar os viajantes, para as pessoas desta era, será entretenimento, para nós será nossa missão, até breve.'
 inputContainer = document.querySelector('.saud')
 
 vel = 40
