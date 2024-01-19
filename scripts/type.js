851c576b1bc2681eb75bcec70c0901ec71081e16
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-18 21:03:31 -0300

    agora foi

--- scripts/type.js
+++ scripts/type.js
@@ -1,14 +1,14 @@
 txt = 'Bem vindo, seja você viajate ou não, como posso explicar... Talvez você já tenha escutado algo sobre viagem no tempo né? eu suponho que sim, se este tema te interessa faz sentido que tenha vindo parar aqui. Logo sua memória voltará aos poucos, a minha levou cerca de 15 anos para estabilizar. O site tem o intúito de entreter e atualizar os viajantes, para as pessoas desta era, será entretenimento, para nós será nossa missão, até breve.'
-
+el = document.querySelector(".saud")
 var i = 0;
 
 var speed = 50;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.querySelector(".saud").innerHTML += txt.charAt(i);
+function typeWriter(frase, saida) {
+  if (i < frase.length) {
+    saida.innerHTML += frase.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriter(frase, saida), speed);
   }
 }
-window.addEventListener("load", typeWriter())
+window.addEventListener("load", typeWriter(txt, el))
