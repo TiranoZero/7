9f5a70c30fc562b9ffd41dffb9ded8a675df6367
TiranoZero <147494303+TiranoZero@users.noreply.github.com>
2024-01-19 20:36:17 -0300

    oxi

--- scripts/type.js
+++ scripts/type.js
@@ -1,5 +1,7 @@
 txt = 'Bem vindo, seja você viajate ou não, como posso explicar... Talvez você já tenha escutado algo sobre viagem no tempo né? eu suponho que sim, se este tema te interessa faz sentido que tenha vindo parar aqui. Logo sua memória voltará aos poucos, a minha levou cerca de 15 anos para estabilizar. O site tem o intúito de entreter e atualizar os viajantes, para as pessoas desta era, será entretenimento, para nós será nossa missão, até breve.'
 el = document.querySelector(".saud")
+txt2 = "Transmissão Iniciada..."
+el2 = document.querySelector("h2")
 var i = 0;
 
 var speed = 40;
@@ -15,7 +17,7 @@ function typeWriter(frase, saida) {
 }
 
 function main(){
-  typeWriter("Transmissão Iniciada...",document.querySelector("h2.title"))
+  typeWriter(txt2, el2)
   typeWriter(txt, el)
 }
 
